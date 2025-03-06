5ceb8d6e2c9d8d25c7a66697ed4be895bc0e916e
carlosHenrique <carlosgracom389@gmail.com>
2025-03-05 21:20:21 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas, ao jogo do numero secreto!')
+let numeroSecreto = 5;
+let numeroEscolhido = prompt('Escolha um numero entre 1 e 10')
+
+if (numeroSecreto == numeroEscolhido) {
+    console.log('Parabens!! Voce descobriu o numero secreto.')
+}
